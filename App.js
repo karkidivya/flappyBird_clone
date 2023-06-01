83e63f1c92fafafba39643b60f62fd81c6f85d2d
Divya Karki <karkidivya5@.com>
2023-06-01 18:59:49 +0545

    setup_complete

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello this is divya karki speaking</Text>
+      <Text>hello this is divya karki speaking</Text>
       <StatusBar style="auto" />
     </View>
   );
